2dc365872d4d645229dd6909da9064a0f0f3cfea
Jonathan Hirz <jonathanhirz@gmail.com>
2015-07-12 23:55:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+use this project/repo to make changes to the default empty project folder
+
+to create a new project, clone this project [git@bitbucket.org:jonathanhirz/_empty.git] into a new folder, or copy paste folder, doesn't matter.
+remember, git folder comes with it either way, so you need to point git/remote to the new repo before pushing changes.
---
+++ src/MenuState.hx
@@ -0,0 +1,29 @@
+import luxe.States;
+
+class MenuState extends State {
+
+    public function new(_name:String) {
+        super({ name:_name });
+    } //new
+
+    override function init() {
+
+
+    } //init
+
+    override function onenter<T>(_value:T) {
+
+
+    } //onenter
+
+    override function onleave<T>(_value:T) {
+
+
+    } //onleave
+
+    override function update(dt:Float) {
+
+
+    } //update
+
+} //MenuState
