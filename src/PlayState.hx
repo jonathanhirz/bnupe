9da6840d7d30baa8f61d28b3e9f5ba73a9b0b38a
Jonathan Hirz <Jonathan@jonathans-air.attlocal.net>
2015-07-27 20:43:52 -0700

    sync

--- src/PlayState.hx
+++ src/PlayState.hx
@@ -48,11 +48,29 @@ class PlayState extends State {
         player_desired_position = player_component.desired_position;
 
         // get_surrounding_tiles_at_position() [build an array of the 8 tiles around the player, organized]
-        // for each tile...
+        // check_for_and_resolve_collisions() [check for a collision with player/world, check where that collision lands in the array, resolve accordingly]
+
 
         player.pos = player_desired_position;
 
 
     } //update
+
+    function get_surrounding_tiles_at_position(_player_tile_position:Vector) {
+        // take the player's tile position, build an array of the 8 surrounding tiles (not including the one he's on)
+        // organize the tiles in the correct order (check the tutorial)
+        // return an array that we can pass into the check collisions function
+
+    } //get_surrounding_tiles_at_position
+
+    function check_for_and_resolve_collisions(_player:Sprite) {
+        // get player's bounding box collider
+        // for every collision between the player and a tile in the array,
+        // check the position in the array of the collision (this gives priority)
+        // resolve accordingly, update desired position
+        // update player position
+
+
+    } //check_for_and_resolve_collisions
 
 } //PlayState
